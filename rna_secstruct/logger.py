1c63d1370360c713a2db3a33fe4039de855c8126
Joseph Yesselman <jyesselm@unl.edu>
2024-01-09 13:11:06 -0600

    fixed logger issue

---
+++ rna_secstruct/logger.py
@@ -0,0 +1,37 @@
+import logging
+import sys
+
+# logging #####################################################################
+
+APP_LOGGER_NAME = "SETUP"
+
+
+def setup_applevel_logger(logger_name=APP_LOGGER_NAME, is_debug=False, file_name=None):
+    """
+    Set up the logger for the app
+    """
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG if is_debug else logging.INFO)
+
+    formatter = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+
+    # pylint: disable=C0103
+    sh = logging.StreamHandler(sys.stdout)
+    sh.setFormatter(formatter)
+    logger.handlers.clear()
+    logger.addHandler(sh)
+
+    if file_name:
+        # pylint: disable=C0103
+        fh = logging.FileHandler(file_name)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
+    return logger
+
+
+def get_logger(module_name):
+    """
+    Get the logger for the module
+    """
+    return logging.getLogger(APP_LOGGER_NAME).getChild(module_name)
--- setup.py
+++ setup.py
@@ -8,46 +8,44 @@ try:
 except ImportError:
     from distutils.core import setup
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
     sys.exit()
 
-readme = open('README.md').read()
+readme = open("README.md").read()
 ## Documentation
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-        name='rna_secstruct',
-        version='0.1.0',
-        description='a minimal package for parsing and editing rna secondary structure',
-        long_description=readme,
-        long_description_content_type="test/markdown",
-        author='Joe Yesselman',
-        author_email='jyesselm@unl.edu',
-        url='https://github.com/jyesselm/rna_secstruct',
-        packages=[
-            'rna_secstruct',
-        ],
-        package_dir={'rna_secstruct': 'rna_secstruct'},
-        py_modules=[
-            'rna_secstruct/motif',
-            'rna_secstruct/parser',
-            'rna_secstruct/secstruct',
-        ],
-        include_package_data=True,
-        install_requires=requirements,
-        zip_safe=False,
-        keywords='rna_secstruct',
-        classifiers=[
-            'Intended Audience :: Developers',
-            'Natural Language :: English',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: Implementation :: PyPy',
-        ],
-        entry_points={
-            'console_scripts': [
-            ]
-        }
+    name="rna_secstruct",
+    version="0.1.0",
+    description="a minimal package for parsing and editing rna secondary structure",
+    long_description=readme,
+    long_description_content_type="test/markdown",
+    author="Joe Yesselman",
+    author_email="jyesselm@unl.edu",
+    url="https://github.com/jyesselm/rna_secstruct",
+    packages=[
+        "rna_secstruct",
+    ],
+    package_dir={"rna_secstruct": "rna_secstruct"},
+    py_modules=[
+        "rna_secstruct/logger",
+        "rna_secstruct/motif",
+        "rna_secstruct/parser",
+        "rna_secstruct/secstruct",
+    ],
+    include_package_data=True,
+    install_requires=requirements,
+    zip_safe=False,
+    keywords="rna_secstruct",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    entry_points={"console_scripts": []},
 )
